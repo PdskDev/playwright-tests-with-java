baa39558a4a04d38f443f293adad8922c1342b7d
NadetDev <nadetdev@gmail.com>
2025-01-09 12:39:00 +0100

    refacto before/after each test

--- src/test/java/me/nadetdev/playwright/SimpleTest.java
+++ src/test/java/me/nadetdev/playwright/SimpleTest.java
@@ -3,42 +3,45 @@ package me.nadetdev.playwright;
 import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class SimpleTest {
+  private Playwright playwright;
+  private Browser browser;
+  private Page page;
 
-  @Test
-  void shouldShowThePageTitle() {
-    Playwright playwright = Playwright.create();
-    Browser browser = playwright.chromium().launch();
-    Page page = browser.newPage();
+  @BeforeEach
+  void setup(){
+    playwright = Playwright.create();
+    browser = playwright.chromium().launch();
+    page = browser.newPage();
+  }
 
-    page.navigate("https://practicesoftwaretesting.com/");
-    String pageTitle = page.title();
-
-    Assertions.assertTrue(pageTitle.contains("Practice Software Testing"));
-
+  @AfterEach
+  void teardown(){
     browser.close();
     playwright.close();
   }
 
   @Test
-  void shouldSearchByKeyword(){
-    Playwright playwright = Playwright.create();
-    Browser browser = playwright.chromium().launch();
-    Page page = browser.newPage();
-
+  void shouldShowThePageTitle() {
     page.navigate("https://practicesoftwaretesting.com/");
+    String pageTitle = page.title();
 
+    Assertions.assertTrue(pageTitle.contains("Practice Software Testing"));
+  }
+
+  @Test
+  void shouldSearchByKeyword(){
+    page.navigate("https://practicesoftwaretesting.com/");
     page.locator("[placeholder=Search]").fill("Pliers");
     page.locator("button:has-text('Search')").click();
 
     int matchingSearchResults = page.locator(".card").count();
 
     Assertions.assertTrue(matchingSearchResults > 0);
-
-    browser.close();
-    playwright.close();
   }
 }
