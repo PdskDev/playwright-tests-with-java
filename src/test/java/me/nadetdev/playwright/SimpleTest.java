e1dea8a366b4015bdbade69bd74bfca0887b5f29
NadetDev <nadetdev@gmail.com>
2025-01-09 13:04:40 +0100

    Browser options

--- src/test/java/me/nadetdev/playwright/SimpleTest.java
+++ src/test/java/me/nadetdev/playwright/SimpleTest.java
@@ -1,16 +1,43 @@
 package me.nadetdev.playwright;
 
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
-import com.microsoft.playwright.junit.UsePlaywright;
+import com.microsoft.playwright.Playwright;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
-
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-@UsePlaywright
+import java.util.Arrays;
+
 public class SimpleTest {
+  private Playwright playwright;
+  private Browser browser;
+  private Page page;
+
+  @BeforeEach
+  void setup() {
+    playwright = Playwright.create();
+    browser =
+        playwright
+            .chromium()
+            .launch(
+                new BrowserType.LaunchOptions()
+                    .setArgs(Arrays.asList("--no-sandbox", "--disable-gpu", "--disable-extensions"))
+                    .setHeadless(true)
+            );
+    page = browser.newPage();
+  }
+
+  @AfterEach
+  void teardown() {
+    browser.close();
+    playwright.close();
+  }
 
   @Test
-  void shouldShowThePageTitle(Page page) {
+  void shouldShowThePageTitle() {
     page.navigate("https://practicesoftwaretesting.com/");
     String pageTitle = page.title();
 
@@ -18,7 +45,7 @@ public class SimpleTest {
   }
 
   @Test
-  void shouldSearchByKeyword(Page page){
+  void shouldSearchByKeyword() {
     page.navigate("https://practicesoftwaretesting.com/");
     page.locator("[placeholder=Search]").fill("Pliers");
     page.locator("button:has-text('Search')").click();
