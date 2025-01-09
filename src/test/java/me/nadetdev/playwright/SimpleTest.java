a276a16c63014d14d634b7d23399f764a025feef
NadetDev <nadetdev@gmail.com>
2025-01-09 12:29:53 +0100

    Simple test init commit

---
+++ src/test/java/me/nadetdev/playwright/SimpleTest.java
@@ -0,0 +1,45 @@
+package me.nadetdev.playwright;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class SimpleTest {
+
+  @Test
+  void shouldShowThePageTitle() {
+    Playwright playwright = Playwright.create();
+    Browser browser = playwright.chromium().launch();
+    Page page = browser.newPage();
+
+    page.navigate("https://practicesoftwaretesting.com/");
+    String pageTitle = page.title();
+
+    Assertions.assertTrue(pageTitle.contains("Practice Software Testing"));
+
+    browser.close();
+    playwright.close();
+  }
+
+  @Test
+  void shouldSearchByKeyword(){
+    Playwright playwright = Playwright.create();
+    Browser browser = playwright.chromium().launch();
+    Page page = browser.newPage();
+
+    page.navigate("https://practicesoftwaretesting.com/");
+
+    page.locator("[placeholder=Search]").fill("Pliers");
+    page.locator("button:has-text('Search')").click();
+
+    int matchingSearchResults = page.locator(".card").count();
+
+    Assertions.assertTrue(matchingSearchResults > 0);
+
+    browser.close();
+    playwright.close();
+  }
+}
