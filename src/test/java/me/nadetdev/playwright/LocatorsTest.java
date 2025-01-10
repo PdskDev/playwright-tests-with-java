ecf689cad092509b3143564aa6c156a1bd132636
NadetDev <nadetdev@gmail.com>
2025-01-10 12:38:30 +0100

    CSS locators strategies

--- src/test/java/me/nadetdev/playwright/LocatorsTest.java
+++ src/test/java/me/nadetdev/playwright/LocatorsTest.java
@@ -2,10 +2,12 @@ package me.nadetdev.playwright;
 
 import com.microsoft.playwright.*;
 import com.microsoft.playwright.assertions.PlaywrightAssertions;
+import com.microsoft.playwright.options.AriaRole;
 import com.microsoft.playwright.options.LoadState;
-import org.junit.jupiter.api.*;
-
 import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.*;
 
 public class LocatorsTest {
   protected static Playwright playwright;
@@ -98,6 +100,16 @@ public class LocatorsTest {
       page.getByPlaceholder("Your last name").fill("Wright");
       page.getByPlaceholder("email").fill("test@email.com");
     }
+  }
+
+  @DisplayName("Locating element by TestId")
+  @Nested
+  class LocaltingElementsByTestId {
+
+    @BeforeEach
+    void openTheCatalogPage() {
+      openPage();
+    }
 
     @DisplayName("Locate element by test id")
     @Test
@@ -109,13 +121,148 @@ public class LocatorsTest {
       PlaywrightAssertions.assertThat(page.getByTestId("search-caption"))
           .containsText("Searched for: pliers");
     }
+  }
 
-    @DisplayName("Locate element by title")
+  @DisplayName("Locating element by multiple match")
+  @Nested
+  class LocaltingElementsWithMultiplematch {
+
+    @BeforeEach
+    void openTheCatalogPage() {
+      openPage();
+    }
+
+    @DisplayName("Locate element by first match")
     @Test
-    void byTitle() {
-      page.getByAltText("Bolt Cutters").click();
-      page.getByTitle("Practice Software Testing - Toolshop").click();
+    void byFirstElement() {
+      page.locator(".card").first().click();
       PlaywrightAssertions.assertThat(page.getByText("Combination Pliers")).isVisible();
     }
+
+    @DisplayName("Locate element by last match")
+    @Test
+    void byLastElement() {
+      page.locator(".card").last().click();
+      PlaywrightAssertions.assertThat(page.getByText("Thor Hammer")).isVisible();
+    }
+
+    @DisplayName("Locate element by nth match")
+    @Test
+    void byNthElement() {
+      page.locator(".card").nth(4).click();
+      PlaywrightAssertions.assertThat(page.getByText("Slip Joint Pliers")).isVisible();
+    }
+
+    @DisplayName("Locate element by all matchs")
+    @Test
+    void byAllElements() {
+      playwright.selectors().setTestIdAttribute("data-test");
+      //List<String> itemNames = page.getByTestId("product-name").allTextContents();
+      page.getByTestId("product-name").nth(5).click();
+      PlaywrightAssertions.assertThat(page.getByText("Claw Hammer with Shock Reduction Grip")).isVisible();
+    }
+  }
+
+  @DisplayName("Locating element by CSS Selectors")
+  @Nested
+  class LocaltingElementsByCSSSelectors {
+
+    @BeforeEach
+    void openTheCatalogPage() {
+      openPage();
+    }
+
+    @DisplayName("Locate element by tag + attribute")
+    @Test
+    void byCssTagAndAttribute() {
+      page.locator("img[alt='Combination Pliers']").click();
+      PlaywrightAssertions.assertThat(page.getByText("Combination Pliers")).isVisible();
+    }
+
+    @DisplayName("Locate element by CSS selector")
+    @Test
+    void byCssClassSelector() {
+      PlaywrightAssertions.assertThat(page.locator(".img-fluid")).isVisible();
+    }
+
+    @DisplayName("Locate element by CSS selector")
+    @Test
+    void byCssHasText() {
+      page.locator("a:has-text('Sign in')").click();
+      PlaywrightAssertions.assertThat(page.locator("h3:has-text('Login')")).isVisible();
+    }
+
+    @DisplayName("Locate element by CSS Id")
+    @Test
+    void byCssId() {
+      page.locator("#language").click();
+      page.locator("div[class='btn-group dropdown'] li:nth-child(4) a:nth-child(1)").click();
+      PlaywrightAssertions.assertThat(page.locator(".grid-title").nth(1)).containsText("Fourchette de prix");
+    }
+
+
+  }
+
+  @DisplayName("Locating element by CSS Selectors")
+  @Nested
+  class LocaltingElementsUsingCSS {
+
+    @BeforeEach
+    void openTheCatalogPage() {
+      page.navigate("https://practicesoftwaretesting.com/contact");
+    }
+
+    @DisplayName("Locate input text by Id")
+    @Test
+    void locateInputFirstNameById() {
+      page.locator("#first_name").fill("NadetDev");
+      PlaywrightAssertions.assertThat(page.locator("#first_name")).hasValue("NadetDev");
+    }
+
+    @DisplayName("Locate button by Css class")
+    @Test
+    void locateButtonByCssClass() {
+      page.locator("#first_name").fill("NadetDev");
+      page.locator(".btnSubmit").click();
+      List<String> alertMessages = page.locator(".alert").allTextContents();
+      //PlaywrightAssertions.assertThat(page.locator("#first_name")).hasValue("NadetDev");
+      Assertions.assertFalse(alertMessages.isEmpty());
+    }
+
+    @DisplayName("Locate input text by attribute")
+    @Test
+    void locateInputByAttribute() {
+      page.locator("[placeholder='Your last name *']").fill("Fashion Dev");
+
+      PlaywrightAssertions.assertThat(page.locator("[placeholder='Your last name *']")).hasValue("Fashion Dev");
+
+      page.locator(".btnSubmit").click();
+      List<String> alertMessages = page.locator(".alert").allTextContents();
+
+      Assertions.assertFalse(alertMessages.isEmpty());
+    }
+
+    @DisplayName("Locate list and filter element")
+    @Test
+    void locateListAndFilterElement() {
+      page.getByText("Home").click();
+      page.getByLabel("Hammer").click();
+
+      PlaywrightAssertions.assertThat(page.getByLabel("Hammer")).isChecked();
+
+      page.waitForSelector("h5:has-text('Claw Hammer with Shock Reduction Grip')");
+
+      PlaywrightAssertions.assertThat(page.locator("h5:has-text('Claw Hammer with Shock Reduction Grip')")).isVisible();
+
+      /*List<String> listProducts =
+          page.locator(".card-title")
+                  .filter(new Locator.FilterOptions().setHasText("claw"))
+                  .allTextContents();
+
+      Assertions.assertFalse(listProducts.isEmpty());
+      Assertions.assertEquals(3, listProducts.size());*/
+    }
+
+
   }
 }
